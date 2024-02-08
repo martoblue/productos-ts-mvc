9eea095ce2de42a9317683e163e3d80018d42ed0
Martorelli <luis.martorelli@bm-maritima.cl>
2024-02-08 16:47:42 -0300

    operaciones CRUD del modelo, terminada

---
+++ database/docs/levantar.md
@@ -0,0 +1,5 @@
+## Para levantar el contenedor de Mysql
+
+```sh
+docker-compose up -d
+```
--- src/models/products.js
+++ src/models/products.js
@@ -1,5 +1,51 @@
 const db = require('../config/database');
 
-class Product {}
+class Product {
+  static findAll() {
+    return new Promise((resolve, reject) => {
+      db.query('SELECT * FROM products', (error, results) => {
+        if (error) return reject(error);
+        resolve(results);
+      });
+    });
+  }
+
+  static findById(id) {
+    return new Promise((resolve, reject) => {
+      db.query('SELECT * FROM WHERE id = ?', [id], (error, results) => {
+        if (error) return reject(error);
+        resolve(results[0]);
+      });
+    });
+  }
+
+  static create(newProduct) {
+    return new Promise((resolve, reject) => {
+      db.query('INSERT INTO products SET ?', newProduct, (error, results) => {
+        if (error) return reject(error);
+
+        resolve(results.insertId);
+      });
+    });
+  }
+
+  static update(id, updatedProduct) {
+    return new Promise((resolve, reject) => {
+      db.query('UPDATE products SET ? WHERE id = ?', [updatedProduct, id], (error, results) => {
+        if (error) return reject(error);
+        resolve(results);
+      });
+    });
+  }
+
+  static delete(id) {
+    return new Promise((resolve, reject) => {
+      db.query('DELETE FROM products SET WHERE id = ?', [id], (error, results) => {
+        if (error) return reject(error);
+        resolve(results);
+      });
+    });
+  }
+}
 
 module.exports = Product;
